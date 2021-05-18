cd9d0953f88bf124898ce319fc9c7c645734c098
ericfleishman <ericfleishmanwork@gmail.com>
2021-05-18 16:34:59 -0500

    Updated sleep time

--- driverTool.py
+++ driverTool.py
@@ -9,7 +9,7 @@ def searchTweets(username, password, searchterm):
     driver = Chrome(ChromeDriverManager().install())
     driver.get('https://twitter.com/login')
 
-    time.sleep(2)
+    time.sleep(1)
 
     user = driver.find_element_by_xpath("//input[@name='session[username_or_email]']")
     user.send_keys(username)
@@ -18,22 +18,22 @@ def searchTweets(username, password, searchterm):
     passw.send_keys(password)
     passw.send_keys(Keys.RETURN)
 
-    time.sleep(2)
+    time.sleep(1)
 
     search_input = driver.find_element_by_xpath("//input[@aria-label='Search query']")
     search_input.send_keys(searchterm)
     search_input.send_keys(Keys.RETURN)
 
-    time.sleep(2)
+    time.sleep(1)
 
     people = driver.find_element_by_link_text('People')
     people.click()
 
-    time.sleep(2)
+    time.sleep(1)
 
     driver.find_element_by_xpath("//div[@class='css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']").click()
 
-    time.sleep(2)
+    time.sleep(1)
 
 
     tweet = driver.find_element_by_xpath("//div[@class='css-901oao r-1fmj7o5 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0']")
