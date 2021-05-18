5ec9d6b65b05a2361e1f4cba338a256fc61ebe03
ericfleishman <ericfleishmanwork@gmail.com>
2021-05-18 16:31:28 -0500

    Scrapes a user's most recent tweet

--- driverTool.py
+++ driverTool.py
@@ -9,66 +9,35 @@ def searchTweets(username, password, searchterm):
     driver = Chrome(ChromeDriverManager().install())
     driver.get('https://twitter.com/login')
 
-    wait = True #Prevents code from executing prior to page load
+    time.sleep(2)
 
-    while wait is True: #Logs into twitter
-        try:
-            user = driver.find_element_by_xpath("//input[@name='session[username_or_email]']")
-            user.send_keys(username)
+    user = driver.find_element_by_xpath("//input[@name='session[username_or_email]']")
+    user.send_keys(username)
 
-            passw = driver.find_element_by_xpath("//input[@name='session[password]']")
-            passw.send_keys(password)
-            passw.send_keys(Keys.RETURN)
+    passw = driver.find_element_by_xpath("//input[@name='session[password]']")
+    passw.send_keys(password)
+    passw.send_keys(Keys.RETURN)
 
-            wait = False
-        except:
-            pass
+    time.sleep(2)
 
-    wait = True
+    search_input = driver.find_element_by_xpath("//input[@aria-label='Search query']")
+    search_input.send_keys(searchterm)
+    search_input.send_keys(Keys.RETURN)
 
-    while wait is True: #Uses the twitter search query
-        try:
-            search_input = driver.find_element_by_xpath("//input[@aria-label='Search query']")
-            search_input.send_keys(searchterm)
-            search_input.send_keys(Keys.RETURN)
-            wait = False
-        except:
-            pass
+    time.sleep(2)
 
-    wait = True
+    people = driver.find_element_by_link_text('People')
+    people.click()
 
-    while wait is True:
-        try:
-            people = driver.find_element_by_link_text('People')
-            people.click()
-            time.sleep(3)
-            wait = False
-        except:
-            pass
+    time.sleep(2)
 
-    wait = True
+    driver.find_element_by_xpath("//div[@class='css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']").click()
 
-    while wait is True:
-        try:
-            driver.find_element_by_xpath("//div[@class='css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']").click()
-            wait = False
-        except:
-            pass
-
-    wait = True
-
-    while wait is True:
-        try:
-           tweet = driver.find_element_by_xpath("//div[@class='css-901oao r-1fmj7o5 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0']")
-           print(tweet.text)
-           wait = False
-        except:
-            pass
+    time.sleep(2)
 
 
+    tweet = driver.find_element_by_xpath("//div[@class='css-901oao r-1fmj7o5 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0']")
 
-    wait = True
-    while wait is True:
-        ctr = 0
-        ctr += 1
+    return tweet.text
+
 
--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 #Needs relevant inputs for main.py
 import driverTool
 
-driverTool.searchTweets('username','password','elonmusk')
+elontweets = []
+
+elontweet = driverTool.searchTweets('username','password','elonmusk')
+ifinlistelon = elontweets.count(elontweet)
+
+if ifinlistelon == 0:
+    elontweets.append(elontweet)
+else:
+    pass
+
+print(elontweets)
+
+jakepaultweets = []
+jakepaultweet = driverTool.searchTweets('username','password','jakepaul')
+ifinlistjakepaul = jakepaultweets.count(jakepaultweet)
+
+if ifinlistjakepaul == 0:
+    jakepaultweets.append(jakepaultweet)
+else:
+    pass
+
+print(jakepaultweets)
+
+#Now Has a way of parsing various users most recent tweet, now needs certain key words
+#to determine if it has anything to do with crypto.
